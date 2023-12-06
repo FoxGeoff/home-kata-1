62c4b1b6f87379ed834c4c2fcb533467ebbb7843
Geoff Fox <fox.geoffrey@gmail.com>
2023-12-06 13:31:31 -0800

    Step 3 - Create a test house for your app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -13,7 +14,7 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
       <button class="primary" type="button">Search</button>
     </form>
   </section>
-
+<!--home.location -->
   <section class="results">
     <app-housing-location></app-housing-location>
   </section>
@@ -22,4 +23,17 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 })
 export class HomeComponent {
 
+  // Add test item
+    readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+    housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
